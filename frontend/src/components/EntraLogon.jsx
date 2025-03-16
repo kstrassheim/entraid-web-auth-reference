9f80b2c480b7611c76d10b8f8d127427ea66008e
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2025-03-16 17:15:53 +0100

    made nav more nice

--- frontend/src/components/EntraLogon.jsx
+++ frontend/src/components/EntraLogon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useMsal, AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
+import { useMsal , AuthenticatedTemplate, UnauthenticatedTemplate} from '@azure/msal-react';
 import { loginRequest } from './azureAuth';
 
 const EntraLogon = () => {
@@ -10,28 +10,28 @@ const EntraLogon = () => {
       let loginRequestParam = forcePopup ? { ...loginRequest, prompt: "select_account" } : loginRequest;
       const response = await instance.loginPopup(loginRequestParam);
       instance.setActiveAccount(response.account);
+
     } catch (error) {
       console.error("Login failed:", error);
     }
   };
 
+
   const logoutFunc = async () => {
     await instance.logoutRedirect({
       postLogoutRedirectUri: window.location.origin // or any URL you want users redirected to
     });
-  };
+  }
 
-  return (
-    <div>
+  return <div className="logon-buttons">
       <AuthenticatedTemplate>
-        <a href="#" onClick={logoutFunc} className="nav-link">Sign Out</a>
-        <a href="#" onClick={() => logonFunc(true)} className="nav-link">Change Account</a>
+        <button onClick={logoutFunc}>Sign Out</button>
+        <button onClick={()=>logonFunc(true)}>Change Account</button>
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
-        <a href="#" onClick={() => logonFunc()} className="nav-link">Sign In</a>
+        <button onClick={logonFunc}>Sign In</button>
       </UnauthenticatedTemplate>
     </div>
-  );
 };
 
 export default EntraLogon;
