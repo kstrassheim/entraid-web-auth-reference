686571b49d2f8f7215c19fa8c196c0213184f77e
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2025-03-22 17:54:58 +0100

    added working modules

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,8 @@
+# get api role dictionaries
+module api_roles {
+  source = "./modules/azure_api_roles"
+}
+
 data "azurerm_resource_group" "rg" {
   name = "entraid-web-auth-reference-dev"   // name of your resource group
 }
@@ -94,18 +99,18 @@ resource "azuread_application" "reg" {
     resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
 
     resource_access {
-      id   = lookup(local.user_permissions_lookup,"User.Read.All")
-      type = lookup(local.azure_app_permission_types, "Delegated")
+      id   = lookup(module.api_roles.user_roles_dictionary,"User.Read.All")
+      type = lookup(module.api_roles.role_type_mapping, "Delegated")
     }
 
     resource_access {
-      id   = lookup(local.user_permissions_lookup,"Group.Read.All")
-      type = lookup(local.azure_app_permission_types, "Delegated")
+      id   = lookup(module.api_roles.user_roles_dictionary,"Group.Read.All")
+      type = lookup(module.api_roles.role_type_mapping, "Delegated")
     }
 
     resource_access {
-      id   = lookup(local.app_permissions_lookup,"Directory.Read.All")
-      type = lookup(local.azure_app_permission_types, "Application")
+      id   = lookup(module.api_roles.application_roles_dictionary,"Directory.Read.All")
+      type = lookup(module.api_roles.role_type_mapping, "Application")
     }
   }
 
@@ -135,64 +140,3 @@ resource "azuread_service_principal" "enterprise" {
   # Allow only assigned users to login to this application
   app_role_assignment_required  = true
 }
-
-output "web_url" {
-  value = "https://${azurerm_linux_web_app.web.default_hostname}"
-  description = "The URL of the deployed web app"
-}
-
-output "application_insights_instrumentation_key" {
-  value = nonsensitive(azurerm_application_insights.log.instrumentation_key)
-}
-
-output "application_insights_connection_string" {
-  description = "The connection string for Application Insights"
-  value       = nonsensitive(azurerm_application_insights.log.connection_string)
-}
-
-output "client_id" {
-  description = "The Client ID for logon"
-  value       = azuread_application.reg.client_id
-}
-
-output "tenant_id" {
-  description = "The Tenant for the logon"
-  value       =  azuread_service_principal.enterprise.application_tenant_id
-}
-
-output "oauth2_permission_scope_uri" {
-  description = "The full URI for the defined OAuth2 permission scope"
-  value       = "api://${azuread_application.reg.client_id}/${tolist(azuread_application.reg.api[0].oauth2_permission_scope)[0].value}"
-}
-
-output "app_roles_allowed_member_types_list" {
-  description = "List of allowed member types for each app role"
-  value = join(", ", distinct(flatten([
-    for role in azuread_application.reg.app_role : tolist(role.allowed_member_types)
-  ])))
-}
-
-output "requested_graph_api_delegated_permissions" {
-    value = distinct(compact(flatten([
-    for rra in azuread_application.reg.required_resource_access : [
-      for ra in rra.resource_access : ra.type == "Scope" ? lookup(local.user_permissions_lookup_rev, ra.id, "unknown") : null
-    ]
-  ])))
-}    
-
-output "requested_graph_api_application_permissions" {
-    value = distinct(compact(flatten([
-    for rra in azuread_application.reg.required_resource_access : [
-      for ra in rra.resource_access : ra.type == "Role" ? lookup(local.app_permissions_lookup_rev, ra.id, "unknown") : null
-    ]
-  ])))
-} 
-# resource "local_file" "outputs_json" {
-#   content = jsonencode({
-#     web_url              = "https://${azurerm_linux_web_app.web.default_hostname}"
-#     instrumentation_key  = azurerm_application_insights.log.instrumentation_key
-#     client_id            = azuread_application.reg.client_id
-#     tenant_id            = azuread_service_principal.enterprise.application_tenant_id
-#   })
-#   filename = "${path.module}/terraform_output.json"
-# }
