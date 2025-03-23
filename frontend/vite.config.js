22beacb9de587a37d7e49ad96397812fd06b1440
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2025-03-23 19:13:39 +0100

    added static copy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import copy from 'vite-plugin-static-copy'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tfconfig from './terraform.config.json'
 
 export default defineConfig({
   plugins: [
     react(),
-    envLogoPlugin(),
-    copy({
+    viteStaticCopy({
       targets: [
         // Apply logo flip on test and prod
-        { src: `logo_src/(${tfconfig.env.value})/*.png`, dest: 'public' }
-        { src: `logo_src/(${tfconfig.env.value})/favicon.ico`, dest: 'public' }
+        { src: `logo_src/(${tfconfig.env.value})/*.png`, dest: 'public' },
+        { src: `logo_src/(${tfconfig.env.value})/favicon.ico`, dest: 'public' },
         { src: `logo_src/(${tfconfig.env.value})/logo.png`, dest: 'src/assets' }
       ]
     })
